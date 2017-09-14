e9fd60e96733d5565f680146cb5a6ff29ee2b09b
Jacob Garby <j.garby@icloud.com>
2017-09-14 18:12:26 +0100

    fixed a number of bugs related to typing and the cursor

--- kernel.c
+++ kernel.c
@@ -29,6 +29,7 @@ const char *prompt = "<mustard> ";
 const unsigned int prompt_length = 10; // amount of characters in the prompt
 const unsigned short int prompt_attr = 0x0e;
 char *command;
+char *command_history[255];
 
 struct IDT_entry {
 	unsigned short int offset_lowerbits;
@@ -39,6 +40,7 @@ struct IDT_entry {
 };
 
 struct IDT_entry IDT[IDT_SIZE];
+
 
 void idt_init(void) {
 	unsigned long keyboard_address;
@@ -52,7 +54,7 @@ void idt_init(void) {
 	IDT[0x21].zero = 0;
 	IDT[0x21].type_attr = INTERRUPT_GATE;
     IDT[0x21].offset_higherbits = (keyboard_address & 0xffff0000) >> 16;
-    
+
 	// ICW1 to begin initialization
 	write_port(0x20 , 0x11);
 	write_port(0xA0 , 0x11);
@@ -69,7 +71,7 @@ void idt_init(void) {
     write_port(0x21 , 0x01);
 	write_port(0xA1 , 0x01);
     // Initialization finished
-    
+
 	write_port(0x21 , 0xff);
 	write_port(0xA1 , 0xff);
 
@@ -78,12 +80,6 @@ void idt_init(void) {
 	idt_ptr[1] = idt_address >> 16 ;
 
 	load_idt(idt_ptr);
-}
-
-void mv_cursor(int desired_location) {
-    vidptr[current_loc + 1] = 0x07;
-    current_loc = desired_location;
-    vidptr[current_loc + 1] = 0x70;
 }
 
 /**
@@ -98,32 +94,32 @@ void keyboard_init(void) {
 /** Prints at the cursor location with a particular attribute */
 void printa(const char *str, const unsigned short int attr) {
     /** Attribute colours:
-     * 0 - Black, 1 - Blue, 2 - Green, 3 - Cyan, 4 - Red, 
-     * 5 - Magenta, 6 - Brown, 7 - Light Grey, 8 - Dark Grey, 
-     * 9 - Light Blue, 10/a - Light Green, 11/b - Light Cyan, 
-     * 12/c - Light Red, 13/d - Light Magenta, 14/e - Light Brown, 
+     * 0 - Black, 1 - Blue, 2 - Green, 3 - Cyan, 4 - Red,
+     * 5 - Magenta, 6 - Brown, 7 - Light Grey, 8 - Dark Grey,
+     * 9 - Light Blue, 10/a - Light Green, 11/b - Light Cyan,
+     * 12/c - Light Red, 13/d - Light Magenta, 14/e - Light Brown,
      * 15/f – White.
-     * 
+     *
      * Notes:
      * Light brown is literally just yellow
      */
 
     unsigned int i = 0;
-    
+
     /**
      * Stop when a null character is found, since strings are null-terminated
      */
 	while (str[i] != '\0') {
-        vidptr[current_loc] = str[i++];
-        vidptr[current_loc + 1] = attr;
-        mv_cursor(current_loc + 2);
+        /** Add the character to the video memory */
+        vidptr[current_loc++] = str[i++];
+        /** Set the attributes of that character */
+		vidptr[current_loc++] = attr;
 	}
 }
 
 void printca(const unsigned char ch, const unsigned short int attr) {
-    vidptr[current_loc] = ch;
-    vidptr[current_loc + 1] = attr;
-    mv_cursor(current_loc + 2);
+    vidptr[current_loc++] = ch;
+    vidptr[current_loc++] = attr;
 }
 
 /** Prints something at the current cursor location */
@@ -137,8 +133,8 @@ void printc(const unsigned char ch) {
 
 /** Moves the cursor to the next line and shows a prompt */
 void nl(void) {
-    unsigned int line_size = BYTES_PER_VID_ELEMENT * COLUMNS;
-    mv_cursor(current_loc + (line_size - current_loc % (line_size)));
+	unsigned int line_size = BYTES_PER_VID_ELEMENT * COLUMNS;
+    current_loc = current_loc + (line_size - current_loc % (line_size));
 }
 
 /** Convenience for newline then writing the prompt */
@@ -156,73 +152,76 @@ void clear_screen(void) {
 	}
 }
 
-int mfloor(float n) {
-    return n - ((int)n % 1);
-}
-
 int index2x(const unsigned int index) {
     return index/2 % COLUMNS;
 }
 
+/**
+ * Call this every time the cursor moves to update the visible cursor
+ */
 void update_cursor_graphic() {
-    unsigned int i = 1;
-    while (i < SCREENSIZE) {
-        if (vidptr[i] == 0x70) {
-            vidptr[i] = 0x07;
-        }
-        vidptr += 2;
-    }
+	unsigned int i = 1;
+	while (i < SCREENSIZE) {
+		if (vidptr[i] == 0x70) {
+			vidptr[i] = 0x07;
+		}
+		i += 2;
+	}
+	vidptr[current_loc + 1] = 0x70;
 }
 
 /** Handles keyboard input, called from kernel.asm */
 void keyboard_handler_main(void) {
     unsigned char status;
-    
+
     /** Will be set to the keycode of the key which is pressed, if a key is pressed */
-	char keycode;
+		char keycode;
 
     /** Sending end-of-interrup acknowledgement */
-	write_port(0x20, 0x20);
+		write_port(0x20, 0x20);
 
     /** Read the status from the keyboard status port */
-	status = read_port(KEYBOARD_STATUS_PORT);
+		status = read_port(KEYBOARD_STATUS_PORT);
 
     /** If the lowest bit of the status is 0, the buffer must be empty so there isn't any data,
      * so only continue with this if there is actually a key pressed
      */
     if (status & 0x01) {
         keycode = read_port(KEYBOARD_DATA_PORT);
-        
-		if (keycode < 0)
-			return;
 
-		if (keycode == ENTER_KEY_CODE) {
-            // submit command
-			np();
-			return;
-        }
-        
-        if (keycode == 14) { // Backspace
-            if (index2x(current_loc) > prompt_length) { // Embedded 'if', so that it returns even if they can't backspace
-                mv_cursor(current_loc - 2);
-                vidptr[current_loc] = ' ';
-                vidptr[current_loc + 1] = 0x70;
-            }
-            return;
-        }
+				if (keycode < 0)
+					return;
 
-        /** If this code is executed, it means the keycode is >= 0, and
-         * it's not one of the special keys which do something different.
-         * In this case, simply write the character to the screen
-         */
-        vidptr[current_loc] = keyboard_map[(unsigned char) keycode];
-        vidptr[current_loc + 1] = 0x07;
-        mv_cursor(current_loc + 2);
+				if (keycode == ENTER_KEY_CODE) {
+          // submit command
+					np();
+					update_cursor_graphic();
+					return;
+		    }
+
+		    if (keycode == 14) { // Backspace
+		        if (index2x(current_loc) > prompt_length) { // Embedded 'if', so that it returns even if they can't backspace
+		            current_loc -= 2;
+		            vidptr[current_loc] = ' ';
+		            vidptr[current_loc + 1] = 0x07;
+		        }
+						update_cursor_graphic();
+		        return;
+		    }
+
+		    /** If this code is executed, it means the keycode is >= 0, and
+		     * it's not one of the special keys which do something different.
+		     * In this case, simply write the character to the screen
+		     */
+		    vidptr[current_loc++] = keyboard_map[(unsigned char) keycode];
+		    vidptr[current_loc++] = 0x07;
     }
+
+    update_cursor_graphic();
 }
 
 void boot(void) {
-	const char *str = "Mustard kernel v0.05a";
+	const char *str = "Mustard kernel v0.06a";
 	clear_screen();
 	printa(str, 0xe0);
 	nl();
@@ -230,11 +229,7 @@ void boot(void) {
 
 	idt_init();
 	keyboard_init();
+	update_cursor_graphic();
 
-    /**
-     * Make it so the user can continuously input
-     */
-	for(;;) {
-
-    }
+	for(;;);
 }
